4e46b727d7364c1fd2b4e92e59417e7beb23b85c
Ashwin Srinath <ashwinsrnth@gmail.com>
2014-01-29 14:22:56 -0500

    Makefile is now local. Added timer

--- src/main.cu
+++ src/main.cu
@@ -2,10 +2,12 @@
 # include <initialise_data.h>
 # include <Thrust3DHeatSolver.h>
 # include <read_data.h>
+# include <sys/time.h>
+
 
 int main(){
 
-    SimData sim = read_data("params.yml");
+    SimData sim = read_data("../params.yml");
     
     initialise_data(sim);
 
@@ -14,12 +16,19 @@ int main(){
 
     solver->initialise();
 
+    struct timeval t1, t2;
+    double elapsedtime;
+
     for(int i=0; i<sim.nsteps; i++){
         solver->take_step();
+
+    gettimeofday(&t2, NULL);
+    elapsedtime = (t2.tv_usec - t1.tv_usec);
+    printf("Temperature update loop took: %f\n microseconds", elapsedtime);
     }
 
     solver->close();
-    solver->write("output.dat");
+    solver->write("../output.dat");
     
     return 0;
 }
